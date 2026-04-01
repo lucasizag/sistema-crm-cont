bf3febb01df5c810f8eb123e2e0b5dd11a6d1e7c
lucasizag <igtsistemas@gmail.com>
2026-04-01 16:08:09 -0300

    asistentes-sincro-tareas2

--- frontend-crm/src/components/AssistantTasksModal.tsx
+++ frontend-crm/src/components/AssistantTasksModal.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { X, Briefcase, Search, Filter, ArrowUpDown } from 'lucide-react';
+import { X, Briefcase, Filter, ArrowUpDown } from 'lucide-react';
 import api from '../api';
 
 interface Props {
